8261af5d30e1890ff6245c4a0564400dc7fab58c
Bryan <bryan.m.kristiono@gmail.com>
2017-09-09 09:11:24 -0400

    working json request

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import {
 } from 'react-native';
 
 export default class App extends React.Component {
-  url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB97a-l-FZyGXWiOBwze-EhGHIPxQazeNc';
   render() {
     return (
       <View style={styles.container}>
@@ -36,32 +35,33 @@ export default class App extends React.Component {
       fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB97a-l-FZyGXWiOBwze-EhGHIPxQazeNc	', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
-        body: {
-          "requests": [
+        body: JSON.stringify({
+          requests: [
             {
-              "image": {
-                "source": {
-                  "imageUri":
+              image: {
+                source: {
+                  imageUri:
                   "http://www.skincareorg.com/wp-content/uploads/2017/02/How-to-Stop-a-Burn-from-Hurting.jpg"
                 }
               },
-              "features": [
+              features: [
                 {
-                  "type": "WEB_DETECTION",
-                  "maxResults": 5
+                  type: "WEB_DETECTION",
+                  maxResults: 5
                 }
               ]
             }
           ]
-        }
+        })
       })
         .then(function (response) {
           return response.json()
         }).then(function (body) {
           console.log(body);
+        }).catch(function (err) {
+          console.log(err);
         });
     }
   }
@@ -92,8 +92,7 @@ export default class App extends React.Component {
       })
     };
     console.log(object);
-    console.log("");
-    fetch(url, object)
+    fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB97a-l-FZyGXWiOBwze-EhGHIPxQazeNc', object)
       .then(function (response) {
         return response.json()
       }).then(function (body) {
