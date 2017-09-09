f540566d42aa25efe83650f21088ac34fc216a42
Bryan <bryan.m.kristiono@gmail.com>
2017-09-09 14:50:30 -0400

    update

--- App.js
+++ App.js
@@ -9,21 +9,27 @@ import {
 } from 'react-native';
 
 export default class App extends React.Component {
+  
   render() {
     return (
       <View style={styles.container}>
 
         <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this._onTake}>
-          <Text>Take a pic!!</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this._onPick}>
-          <Text>Get pic!!</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this._onTake}>
+            <Text>Take a pic!!</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this._onPick}>
+            <Text>Get pic!!</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.getDetails}>
+            <Text>Get Details!!</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -80,10 +86,41 @@ export default class App extends React.Component {
       .then(function (response) {
         return response.json()
       }).then(function (body) {
-        console.log(JSON.stringify(body.responses));
+        console.log(JSON.stringify(body.responses[0].webDetection.webEntities));
       }).catch(function (err) {
         console.log(err);
       });
+  }
+
+  getDetails(uri) {
+  //   // Imports the Google Cloud client library
+    // Vision = require('@google-cloud/vision');
+
+  //   // Instantiates a client
+    // const vision = Vision();
+    // console.log("TET");
+
+  //   // The name of the image file to annotate
+  //   const fileName = uri;
+
+  //   // Prepare the request object
+  //   const request = {
+  //     source: {
+  //       filename: fileName
+  //     }
+  //   };
+
+  //   // Performs label detection on the image file
+  //   vision.labelDetection(request)
+  //     .then((results) => {
+  //       const labels = results[0].labelAnnotations;
+
+  //       console.log('Labels:');
+  //       labels.forEach((label) => console.log(label.description));
+  //     })
+  //     .catch((err) => {
+  //       console.error('ERROR:', err);
+  //     });
   }
 }
 
